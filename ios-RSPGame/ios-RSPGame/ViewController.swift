db33e25678bbaa601d332d84b0813a2db676c557
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-12-04 19:06:16 +0900

    feat: 초기 작업 진행
    초기 작업 진행

--- ios-RSPGame/ios-RSPGame/ViewController.swift
+++ ios-RSPGame/ios-RSPGame/ViewController.swift
@@ -9,92 +9,9 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    @IBOutlet weak var mainLabel: UILabel!
-    
-    @IBOutlet weak var comImageView: UIImageView!
-    @IBOutlet weak var myImageView: UIImageView!
-    
-    @IBOutlet weak var comChoiceLabel: UILabel!
-    @IBOutlet weak var myChoiceLabel: UILabel!
-    
-    var myChoice: Rps = Rps.rock
-    
-    var comChoice: Rps = Rps(rawValue: Int.random(in: 0...2))!
-    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        comImageView.image = #imageLiteral(resourceName: "ready")
-        myImageView.image = #imageLiteral(resourceName: "ready")
-        
-        comChoiceLabel.text = "준비 중 입니다!"
-        myChoiceLabel.text = "준비 중 입니다!"
-    }
-
-    @IBAction func rpsButtonTapped(_ sender: UIButton) {
-        let title = sender.currentTitle!
-        print(title)
-        
-        switch title {
-        case "가위":
-            myChoice = Rps.scissors
-        case "바위":
-            myChoice = Rps.rock
-        case "보":
-            myChoice = Rps.paper
-        default:
-            break
-        }
-    }
-    
-    @IBAction func selectButtonTapped(_ sender: UIButton) {
-        switch comChoice {
-        case Rps.rock:
-            comImageView.image = #imageLiteral(resourceName: "rock")
-            comChoiceLabel.text = "바위"
-        case Rps.scissors:
-            comImageView.image = #imageLiteral(resourceName: "scissors")
-            comChoiceLabel.text = "가위"
-        case Rps.paper:
-            comImageView.image = #imageLiteral(resourceName: "paper")
-            comChoiceLabel.text = "보"
-        }
-        
-        switch myChoice {
-        case Rps.rock:
-            myImageView.image = #imageLiteral(resourceName: "rock")
-            myChoiceLabel.text = "바위"
-        case Rps.scissors:
-            myImageView.image = #imageLiteral(resourceName: "scissors")
-            myChoiceLabel.text = "가위"
-        case Rps.paper:
-            myImageView.image = #imageLiteral(resourceName: "paper")
-            myChoiceLabel.text = "보"
-        }
-        
-        if comChoice == myChoice {
-            mainLabel.text = "비겼어요 !"
-        } else if comChoice == .rock && myChoice == .paper {
-            mainLabel.text = "이겼어요 !"
-        } else if comChoice == .paper && myChoice == .scissors {
-            mainLabel.text = "이겼어요 !"
-        } else if comChoice == .scissors && myChoice == .rock {
-            mainLabel.text = "이겼어요 !"
-        } else {
-            mainLabel.text = "졌네요.."
-        }
-    }
-    
-    @IBAction func resetButtonTapped(_ sender: UIButton) {
-        comImageView.image = #imageLiteral(resourceName: "ready")
-        myImageView.image = #imageLiteral(resourceName: "ready")
-        
-        comChoiceLabel.text = "준비 중 입니다!"
-        myChoiceLabel.text = "준비 중 입니다!"
-        
-        mainLabel.text = "선택하세요 !"
-        
-        comChoice = Rps(rawValue: Int.random(in: 0...2))!
     }
 }
 
