7b702dc11b88fef6b44781d5950daa37b1f44b2f
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-12-04 21:28:14 +0900

    feat: mainLabel 변수 생성
    ViewController 에 mainLabel 변수 생성

--- ios-RSPGame/ios-RSPGame/ViewController.swift
+++ ios-RSPGame/ios-RSPGame/ViewController.swift
@@ -9,9 +9,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var mainLabel: UILabel!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
     }
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
