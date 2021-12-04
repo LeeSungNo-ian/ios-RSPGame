5b0ac9b01f4d91f91def4c0f1379617831405d6e
ian <78950704+LeeSungNo-ian@users.noreply.github.com>
2021-12-04 21:40:40 +0900

    feat: 가위, 바위, 보 Buttons Style Default 값 변경
    가위, 바위, 보 Buttons Style Default 값 변경

--- ios-RSPGame/ios-RSPGame/ViewController.swift
+++ ios-RSPGame/ios-RSPGame/ViewController.swift
@@ -26,6 +26,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func rpsButtonTapped(_ sender: UIButton) {
+        
     }
     
     @IBAction func resetButtonTapped(_ sender: UIButton) {
